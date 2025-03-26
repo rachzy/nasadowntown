6d29738a37a38e8dfc27b5c2160220c3c94c224f
rachzy <henriqueferreira.fox@gmail.com>
2025-03-25 23:48:39 -0300

    feat: implement btn directive

---
+++ apps/www/src/app/directives/btn/nd-btn.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NdBtnDirective } from './nd-btn.directive';
+
+describe('NdBtnDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NdBtnDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ apps/www/src/app/directives/btn/nd-btn.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, HostBinding, input } from '@angular/core';
+
+type BtnVariants = 'primary' | 'secondary' | 'invisible' | 'bordered';
+type BtnSizes = 'sm' | 'md' | 'lg';
+
+const btnVariants: Record<BtnVariants, string> = {
+  primary: 'bg-white text-black',
+  secondary: 'bg-black text-white',
+  invisible: 'bg-transparent text-white',
+  bordered:
+    'bg-transparent text-white border-2 border-opacity-50 border-white' +
+    ' duration-200 ease-in-out hover:border-opacity-100',
+};
+
+const btnSizes = {
+  sm: 'px-12 py-3 text-sm',
+  md: 'px-16 py-4 text-md',
+  lg: 'px-20 py-5 text-lg',
+};
+
+@Directive({
+  selector: '[ndBtn]',
+})
+export class NdBtnDirective {
+  public readonly variant = input<BtnVariants>('primary');
+  public readonly size = input<BtnSizes>('md');
+  public readonly blocked = input<boolean>(false);
+
+  @HostBinding('class') get classes(): string {
+    return `rounded-md ${btnSizes[this.size()]} ${
+      btnVariants[this.variant()]
+    } ${this.blocked() ? 'cursor-not-allowed opacity-50' : ''}`;
+  }
+}
