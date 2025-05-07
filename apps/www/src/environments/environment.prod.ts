b21881b645be1a64d92a1625ecad050c80ee5279
rachzy <henriqueferreira.fox@gmail.com>
2025-05-06 22:22:20 -0300

    fix: update production API URL to include the API path

--- apps/www/src/environments/environment.prod.ts
+++ apps/www/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-15-228-10-144.sa-east-1.compute.amazonaws.com', // Replace with your actual Firebase hosting URL
+  apiUrl: 'http://ec2-15-228-10-144.sa-east-1.compute.amazonaws.com/api',
 };
